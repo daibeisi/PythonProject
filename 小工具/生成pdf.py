884355ee5b507100c1ac72ee0e2d2bfcec89844d
daibeisi <heyares@163.com>
2022-09-21 09:33:21 +0800

    修改生成pdf脚本

--- 小工具/生成pdf.py
+++ 小工具/生成pdf.py
@@ -16,6 +16,7 @@ pdfmetrics.registerFont(TTFont('MSYH', 'C:\\Windows\\Fonts\\msyh.ttc'))
 class GeneratePDF:
     def __init__(self):
         self._style = getSampleStyleSheet()  # 获取所有样式表
+        self._context = list()
 
     def draw_title(self, title: str):
         """绘制标题"""
@@ -23,25 +24,36 @@ class GeneratePDF:
         ct.fontName = 'MSYH'  # 字体名
         # ct.textColor = colors.green  # 字体颜色
         # ct.bold = True
-        return Paragraph(title, ct)
+        self._context.append(Paragraph(title, ct))
 
-    def draw_little_title(self, title: str):
-        """绘制小标题"""
+    def draw_heading1(self, heading1: str):
+        """绘制标题1"""
         ct = self._style['Heading1']
         ct.fontName = 'MSYH'  # 字体名
-        return Paragraph(title, ct)
+        self._context.append(Paragraph(heading1, ct))
+
+    def draw_heading2(self, heading2: str):
+        """绘制标题2"""
+        ct = self._style['Heading2']
+        ct.fontName = 'MSYH'  # 字体名
+        self._context.append(Paragraph(heading2, ct))
+
+    def draw_heading3(self, heading3: str):
+        """绘制标题3"""
+        ct = self._style['Heading3']
+        ct.fontName = 'MSYH'  # 字体名
+        self._context.append(Paragraph(heading3, ct))
 
     def draw_text(self, text: str):
         """绘制正文"""
         ct = self._style['BodyText']
         ct.fontName = 'MSYH'  # 字体名
-        ct.firstLineIndent = 32  # 第一行开头空格
+        ct.firstLineIndent = 20  # 第一行开头空格
         ct.wordWrap = 'CJK'  # 设置自动换行
         ct.alignment = 0  # 左对齐
-        return Paragraph(text, ct)
+        self._context.append(Paragraph(text, ct))
 
-    @staticmethod
-    def draw_table(*args):
+    def draw_table(self, *args):
         """绘制表格"""
         # 列宽度
         col_width = 120
@@ -61,10 +73,9 @@ class GeneratePDF:
             # ('SPAN', (0, 7), (0, 8)),  # 合并第一列五六行
         ]
         table = Table(args, colWidths=col_width, style=style)
-        return table
+        self._context.append(table)
 
-    @staticmethod
-    def draw_bar(bar_data: list, ax: list, items: list):
+    def draw_bar(self, bar_data: list, ax: list, items: list):
         """创建图表"""
         drawing = Drawing(500, 250)
         bc = VerticalBarChart()
@@ -93,34 +104,37 @@ class GeneratePDF:
         leg.colorNamePairs = items
         drawing.add(leg)
         drawing.add(bc)
-        return drawing
+        self._context.append(drawing)
 
-    @staticmethod
-    def draw_img(path):
+    def draw_img(self, path):
         """绘制图片"""
         img = Image(path)  # 读取指定路径下的图片
         img.drawWidth = 5 * cm  # 设置图片的宽度
         img.drawHeight = 8 * cm  # 设置图片的高度
-        return img
+        self._context.append(img)
+
+    def build(self, filename):
+        """创建pdf文件"""
+        doc = SimpleDocTemplate(filename, pagesize=letter)
+        doc.build(self._context)
 
 
 if __name__ == '__main__':
-    # 创建内容对应的空列表
-    content = list()
-    # 添加标题
     pdf = GeneratePDF()
-    content.append(pdf.draw_title('数据分析就业薪资'))
+    # 添加标题
+    pdf.draw_title('数据分析就业薪资')
     # 添加图片
-    # content.append(Graphs.draw_img('抗疫必胜.png'))
+    # pdf.draw_img('抗疫必胜.png')
+    # 添加标题一
+    pdf.draw_heading1('一、大数据岗位现状介绍')
     # 添加段落文字
-    content.append(pdf.draw_text('众所周知，大数据分析师岗位是香饽饽，'
-                                 '近几年数据分析热席卷了整个互联网行业，'
-                                 '与数据分析的相关的岗位招聘、培训数不胜数。'
-                                 '很多人前赴后继，想要参与到这波红利当中。'
-                                 '那么数据分析师就业前景到底怎么样呢？'))
-    # 添加小标题
-    # content.append(Graphs.draw_title(''))
-    content.append(pdf.draw_little_title('不同级别的平均薪资'))
+    pdf.draw_text('众所周知，大数据分析师岗位是香饽饽，'
+                  '近几年数据分析热席卷了整个互联网行业，'
+                  '与数据分析的相关的岗位招聘、培训数不胜数。'
+                  '很多人前赴后继，想要参与到这波红利当中。'
+                  '那么数据分析师就业前景到底怎么样呢？')
+    # 添加标题一
+    pdf.draw_heading1('二、不同级别的平均薪资')
     # 添加表格
     data = [
         ('职位名称', '平均薪资', '较上年增长率'),
@@ -128,14 +142,13 @@ if __name__ == '__main__':
         ('高级数据分析师', '25.5K', '14%'),
         ('资深数据分析师', '29.3K', '10%')
     ]
-    content.append(pdf.draw_table(*data))
+    pdf.draw_table(*data)
+    # 添加标题一
+    pdf.draw_heading1('三、热门城市的就业情况')
     # 生成图表
-    # content.append(Graphs.draw_title(''))
-    content.append(pdf.draw_little_title('热门城市的就业情况'))
     b_data = [(25400, 12900, 20100, 20300, 20300, 17400), (15800, 9700, 12982, 9283, 13900, 7623)]
     ax_data = ['BeiJing', 'ChengDu', 'ShenZhen', 'ShangHai', 'HangZhou', 'NanJing']
     leg_items = [(colors.red, '平均薪资'), (colors.green, '招聘量')]
-    content.append(pdf.draw_bar(b_data, ax_data, leg_items))
-    # 生成pdf文件
-    doc = SimpleDocTemplate('report.pdf', pagesize=letter)
-    doc.build(content)
+    pdf.draw_bar(b_data, ax_data, leg_items)
+    # 生成pdf
+    pdf.build('report.pdf')
