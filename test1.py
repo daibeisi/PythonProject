dee1cbb72b26e19595c6e4de8a7f3f681986a309
daibeisi <heyares@163.com>
2023-07-17 09:48:07 +0800

    修改

--- test1.py
+++ test1.py
@@ -1,14 +1,58 @@
-import time
-import requests
+import pandas as pd
+import numpy as np
+from uiautomation import WindowControl, MenuControl
+import win32gui
+import win32con
+import pyautogui
+import random
 
-url = 'http://www.google.com.hk'
 
-print(time.strftime('%Y-%m-%d %H:%M:%S'))
-try:
-    html = requests.get(url, timeout=5).text
-except requests.exceptions.RequestException as e:
-    print(e)
-else:
-    print('success')
+# 获取窗口句柄并打开窗口
+def getHwnd():
+    hwnd = win32gui.FindWindow('WeChatMainWndForPC', '微信')
+    win32gui.ShowWindow(hwnd, win32con.SW_SHOWMINIMIZED)
+    win32gui.ShowWindow(hwnd, win32con.SW_SHOWNORMAL)
+    win32gui.ShowWindow(hwnd, win32con.SW_SHOW)
+    win32gui.SetForegroundWindow(hwnd)
+    return hwnd
 
-print(time.strftime('%Y-%m-%d %H:%M:%S'))
+
+# 复位（自动回复之后自动点击消息列表第二个聊天窗口）
+def fuwei(hwnd):
+    left, top, right, bottom = win32gui.GetWindowRect(hwnd)
+    leftpoint = left + 155
+    toppoint = top + 150
+    pyautogui.moveTo(leftpoint, toppoint)
+    pyautogui.click()
+    return '已复位'
+
+
+getHwnd()
+wx = WindowControl(Name="微信")
+wx.SwitchToThisWindow()
+hw = wx.ListControl(Name="会话")
+
+# 持续循环监听未读消息
+while 1:
+    try:
+        we = hw.TextControl(searchDepth=4)
+        # 如果存在未读消息
+        if we.Name:
+            random()
+            we.Click(simulateMove=False)
+            # 获取当前最后一条消息
+            last_msg = wx.ListControl(Name='消息').GetChildren()[-1].Name
+            if last_msg == '你好':
+                wx.SendKeys('你好，才是真的好{ENTER}')
+                fuwei(getHwnd())
+            elif last_msg == '123':
+                wx.SendKeys('456{ENTER}')
+                fuwei(getHwnd())
+            elif last_msg == '测试':
+                wx.SendKeys('测试成功{ENTER}')
+                fuwei(getHwnd())
+            else:
+                wx.SendKeys('无法匹配{ENTER}')
+                fuwei(getHwnd())
+    except Exception as exc:
+        print(exc)
